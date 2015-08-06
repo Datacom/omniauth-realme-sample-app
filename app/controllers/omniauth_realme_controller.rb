4c742733a58bf67ec85f3bc745c3048394f9da12
ashlynbaum <a@ashlynbaum.com>
2015-08-06 12:12:55 +1200

    Add redirection to REALME functionality
    Define client credentials in devise initializer, sourced from secrets. Remove functionality
    from omniauthRealmeController because redirect behavior is defined in the
    strategies.rb for the gem.

--- app/controllers/omniauth_realme_controller.rb
+++ app/controllers/omniauth_realme_controller.rb
@@ -1,17 +1,9 @@
 class OmniauthRealmeController < ApplicationController
+
   def submit
-    # Actual code for constructing the message and sending to RealMe
-    # application = Devise.setup.omniauth
-
-    application = OmniAuth::Realme::Application.new(
-                            application_params[:customer_name],
-                            application_params[:app_name],
-                            Rails.application.secrets["omniauth_realme"]["shared_key"])
-    integrator = OmniAuth::Realme::Integrator.new(application)
-    message = integrator.build_message("logon")
-    redirect_to(Rails.application.secrets["omniauth_realme"]["integrator"] + message)
-
+    redirect_to user_omniauth_authorize_path(:realme, logon_type: application_params[:use_case])
   end
+
   def signin
     @realme = Realme.new(
                           customer_name: Rails.application.secrets["omniauth_realme"]["customer_name"],
@@ -21,7 +13,7 @@ class OmniauthRealmeController < ApplicationController
 
   private
 
-  def application_params
-    params.require(:realme).permit(:customer_name, :app_name)
-  end
+    def application_params
+      params.permit(:use_case)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: { sessions: "users/sessions", omniauth_callbacks: "users/omniauth_callbacks" }
+  devise_for :users, path: '/', controllers: { sessions: "users/sessions", omniauth_callbacks: "users/omniauth_callbacks" }
   post 'realme' => 'omniauth_realme#submit'
 
   authenticated :user do
